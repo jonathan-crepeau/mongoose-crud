5be7ff56710b3b270240246d6322b31c7229cedc
jonathan-crepeau <jon.crepeau@gmail.com>
2022-11-08 21:03:23 -0800

    returned first database item

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-    console.log('Application is listening on port 3000.');
-})
+mongoose.connect("mongodb+srv://jonathan-crepeau:hello123@city-cluster.quinohu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    .then(() => console.log('Database connection successful...'))
+    .catch(() => console.log('Database connetion error'));
 
+const citySchema = {
+    name: String,
+    description: String,
+}
+
+const City = mongoose.model(
+    'City',
+    citySchema,
+    'Cities'
+);
+
+City.findOne({name: "Boston"}, (err, foundItem) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(foundItem)
+    }
+});
+
+app.listen(3200, () => {
+    console.log('Application listening on port 3200...');
+});
